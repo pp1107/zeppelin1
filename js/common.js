4fdd894fbf9a3aed4a1146a98daafc4d85493840
DESKTOP-KQ4OP4B\Pavel <pavelstravkins@gmail.com>
2017-03-29 11:18:12 +0300

    123

--- js/common.js
+++ js/common.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
-$(window).on('load', function () {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
-});
 
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
